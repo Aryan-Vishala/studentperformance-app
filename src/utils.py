f6081ce9e8676febcdbcf9d8e293ba7a64e62010
Aryan Vishala <aryanvishala@gmail.com>
2026-02-01 06:11:27 +0530

    Fix model loading using dill with absolute path

--- src/utils.py
+++ src/utils.py
@@ -52,10 +52,16 @@ def evaluate_models(X_train, y_train,X_test, y_test, models, params):
     except Exception as e:
         raise CustomException(e, sys)
     
+
 def load_object(file_path):
     try:
-        with open(file_path, "rb") as  file_obj:
+        base_dir = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "..", "..")
+        )
+        full_path = os.path.join(base_dir, file_path)
+
+        with open(full_path, "rb") as file_obj:
             return dill.load(file_obj)
-        
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
